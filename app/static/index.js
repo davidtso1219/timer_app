474fb49c8ffe5d7534eaa5a70989ecaca1b0136c
david tso <davidtso1219@gmail.com>
2021-04-30 21:38:58 -0700

    Added a test to prevent malicious long names.

--- app/static/index.js
+++ app/static/index.js
@@ -3,6 +3,10 @@ var input = document.getElementById("name-input")
 input.onkeyup = (e) => {
     let name = input.value
     if (e.keyCode === 13 && name.trim() && /\w+/.test(name)){
-        location.href = `/timer/${name}`
+        if (name.length > 10){
+            location.href = `/timer/${name.slice(0, 10)}`
+        } else {
+            location.href = `/timer/${name}`
+        }  
     }
 }
