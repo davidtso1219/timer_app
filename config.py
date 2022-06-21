6b5814d934c9306a4b56171001866af40c199ff0
david tso <davidtso1219@gmail.com>
2022-06-21 17:07:46 +0800

    Nothing really changed

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ the Config class.
 import os
 
 class Config(object):
-    FLASK_APP = os.environ.get('FLASK_APP') 
+    FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV') or 'you-are-not-supposed-to-do-something-with-this'
     FLASK_DEBUG = os.environ.get('FLASK_DEBUG') or 'you-are-not-supposed-to-do-something-with-this'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-are-not-supposed-to-do-something-with-this'
